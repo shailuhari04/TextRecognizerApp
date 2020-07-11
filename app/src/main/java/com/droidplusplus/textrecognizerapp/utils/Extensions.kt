c108b5ff83d29ae26ecc3e4b5331a7c928c4d380
Shailendra <shailendraharinkhede@fitternity.com>
2020-07-11 17:52:13 +0530

    1. View Extension function define.

---
+++ app/src/main/java/com/droidplusplus/textrecognizerapp/utils/Extensions.kt
@@ -0,0 +1,18 @@
+package com.droidplusplus.textrecognizerapp.utils
+
+import android.view.View
+
+/**
+ * View Extension function
+ */
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
